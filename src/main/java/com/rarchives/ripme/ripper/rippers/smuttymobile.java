7f2fcf80e5054017514c621e9b680690b4cee040
Kimberlie03 <kimberlie03@gmail.com>
2016-09-21 11:33:12 -0700

    Update smuttymobile.java

--- src/main/java/com/rarchives/ripme/ripper/rippers/smuttymobile.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/smuttymobile.java
@@ -22,8 +22,8 @@ import com.rarchives.ripme.utils.Http;
  */
 public class smuttymobile extends AlbumRipper {
 
-    private static final String DOMAIN = "smutty.com",
-                                HOST   = "smutty";
+    private static final String DOMAIN = "m.smutty.com",
+                                HOST   = "m.smutty";
 
     public smuttymobile(URL url) throws IOException {
         super(url);
